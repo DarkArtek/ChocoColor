1f420300dfc85d654b333491ef578045f2a60c4f
Luca <luca@ahsubs-design.com>
2020-05-15 22:34:37 +0200

    New Version

--- src/chocobo/core.clj
+++ src/chocobo/core.clj
@@ -20,7 +20,7 @@
           current-data (coloring/find-by-name current)
           target-data (coloring/find-by-name target)]
       (do
-        (println (str "*** ChocoColor 0.1 - https://github.com/DarkArtek/ChocoColor"))
+        (println (str "*** ChocoColor 0.2 - https://github.com/DarkArtek/ChocoColor"))
         (println " ")
         (if
           (and
@@ -28,14 +28,15 @@
             (= current (:name current-data))
             (= target (:name target-data)))
           (do
-            (println (str "---> Da " (:name current-data) " a " (:name target-data) " ecco di cosa avrete bisogno:"))
+            (println (str "---> Da " (:name current-data) " a " (:name target-data) ". Ecco a voi gli ingredienti:"))
             (let [chain (feeding/feed-chain (:rgb current-data) (:rgb target-data))]
               (println (format-list (filter #(not (nil? %)) (feeding/feed-chain-totals chain))))
               (println "---> Date da mangiare al vostro chocobo in questo ordine:")
               (println (format-list (feeding/feed-chain-names chain))))
               (println "---> Ecco fatto. Grazie per aver usato ChocoColor.")
               (println "\n")
-              (println " Venite a trovarci su ffxivita.it"))
+              (println "\n")
+              (println "Venite a trovarci su https://ffxivita.it e su https://bit.ly/discovery-eorzea"))
           (do
             (println "---> Colori Disponibili:")
             (println (format-list (map #(:name %) coloring/color-data)))
